e9a50b1e9780124eb6c449556a0421b3dbbfe15c
jurgen1c <jurgen1c@gmail.com>
2020-12-08 14:38:16 -0600

    Add css loader & file structure

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './style.scss';
 import firebase from 'firebase/app';
 import "firebase/analytics";
 import 'firebase/auth';
-import 'firebase/database';
+import 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAiZ8_iZgAYH3-NfyMdxOVngf3ThIEj1Io",
