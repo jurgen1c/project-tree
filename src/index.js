98a28f0099bd02c1a5e56b6f44fbe31e37251677
jurgen1c <jurgen1c@gmail.com>
2020-12-08 17:01:47 -0600

    Add db get

--- src/index.js
+++ src/index.js
@@ -16,3 +16,37 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
+const db = firebase.firestore();
+db.settings({timestampsInSnapshots: true});
+
+const content = document.querySelector('body');
+const treeList = document.createElement('div');
+
+function renderTree(){
+  let li = document.createElement('li');
+  let title = document.createElement('h3');
+  let description = document.createElement('p');
+  let color = document.createElement('span');
+
+  li.setAttribute('data-id', doc.id);
+  title.textContent = doc.data().title;
+  description.textContent = doc.data().description;
+  color.textContent = doc.data().color;
+
+  li.appendChild(title);
+  li.appendChild(description);
+  li.appendChild(color);
+
+  treeList.appendChild(li);
+
+}
+
+db.collection('trees').get().then((snapshot) => {
+  snapshot.docs.forEach(doc => {
+    renderTree(doc);
+  })
+});
+
+window.addEventListener('load', () => {
+  content.appendChild(treeList);
+});
