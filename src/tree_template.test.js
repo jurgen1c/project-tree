41d85e76e5378688588d1bc5a218ead524103089
jurgen1c <jurgen1c@gmail.com>
2020-12-15 18:17:28 -0600

    Fix eslint errors

--- src/tree_template.test.js
+++ src/tree_template.test.js
@@ -1,25 +1,25 @@
 import Tree from './tree_template';
 
-const test = new Tree('Test Tree', 'Test of tree description', 'red')
+const test = new Tree('Test Tree', 'Test of tree description', 'red');
 
 it('Test class instaziation', () => {
   expect(test).toBeDefined();
-})
+});
 
 it('Test content class', () => {
-  expect(test.content.className).toBe('tree')
-})
+  expect(test.content.className).toBe('tree');
+});
 
 it('Test color assigment', () => {
-  expect(test.content.style.background).toBe('red')
-})
+  expect(test.content.style.background).toBe('red');
+});
 
 it('Test title assigment', () => {
-  let title = test.content.querySelector('h3');
+  const title = test.content.querySelector('h3');
   expect(title.textContent).toBe('Test Tree');
-})
+});
 
 it('Test decription assigment', () => {
-  let description = test.content.querySelector('p');
+  const description = test.content.querySelector('p');
   expect(description.textContent).toBe('Test of tree description');
-})
+});
